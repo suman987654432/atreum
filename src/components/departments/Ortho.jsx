e40aa433bf0fcd35312c88c916f65ec6eb1b3c52
Suman Kumar <sumanqaj9876@gmail.com>
2026-02-13 11:52:22 +0530

    feat: Update Ortho component layout and styling for enhanced usability

--- src/components/departments/Ortho.jsx
+++ src/components/departments/Ortho.jsx
@@ -38,21 +38,21 @@ const Ortho = () => {
                        
                     </div>
                     {/* Right Side Form */}
-                    <div className="w-full lg:w-[500px] max-w-full lg:max-w-[500px] lg:ml-auto">
-                        <div className="bg-[#2C747C80] border-[1px] border-[#FFFFFF61] p-5 sm:p-6 md:p-7 rounded-xl shadow-2xl bg-blur-lg backdrop-blur-md">
-                            <h3 className="text-white font-canela font-normal text-[22px] sm:text-[28px] md:text-[32px] leading-[30px] sm:leading-[32px] md:leading-[36px] tracking-normal text-center">
+                    <div className="w-full lg:w-[450px] max-w-full lg:max-w-[450px] lg:ml-auto">
+                        <div className="bg-[#2C747C80] border-[1px] border-[#FFFFFF61] p-4 sm:p-5 md:p-6 rounded-xl shadow-2xl bg-blur-lg backdrop-blur-md">
+                            <h3 className="text-white font-canela font-normal text-[20px] sm:text-[24px] md:text-[28px] leading-[28px] sm:leading-[30px] md:leading-[34px] tracking-normal text-center">
                                 <span className="font-bold italic">Unconditional care</span> begins
                             </h3>
-                            <h3 className="text-white font-canela font-normal text-[22px] sm:text-[28px] md:text-[32px] leading-[30px] sm:leading-[32px] md:leading-[36px] tracking-normal text-center mb-5 sm:mb-6">
+                            <h3 className="text-white font-canela font-normal text-[20px] sm:text-[24px] md:text-[28px] leading-[28px] sm:leading-[30px] md:leading-[34px] tracking-normal text-center mb-4 sm:mb-5">
                                 with knowing you
                             </h3>
 
-                            <form className="space-y-3">
+                            <form className="space-y-2.5">
                                 <div className="relative">
                                     <input
                                         type="text"
                                         placeholder="Name"
-                                        className="w-full bg-[#ffffff1a] border border-transparent rounded-lg text-white placeholder-white/70 px-4 py-3 text-base focus:outline-none focus:border-white/50 focus:bg-[#ffffff25]  font-sohne"
+                                        className="w-full bg-[#ffffff1a] border border-transparent rounded-lg text-white placeholder-white/70 px-3 py-2.5 text-sm focus:outline-none focus:border-white/50 focus:bg-[#ffffff25]  font-sohne"
                                     />
                                 </div>
 
@@ -60,24 +60,24 @@ const Ortho = () => {
                                     <input
                                         type="tel"
                                         placeholder="Phone Number"
-                                        className="w-full bg-[#ffffff1a] border border-transparent rounded-lg text-white placeholder-white/70 px-4 py-3 text-base focus:outline-none focus:border-white/50 focus:bg-[#ffffff25]  font-sohne"
+                                        className="w-full bg-[#ffffff1a] border border-transparent rounded-lg text-white placeholder-white/70 px-3 py-2.5 text-sm focus:outline-none focus:border-white/50 focus:bg-[#ffffff25]  font-sohne"
                                     />
                                 </div>
 
                                 <div className="relative">
                                     <select
-                                        className="w-full bg-[#ffffff1a] border border-transparent rounded-lg text-white placeholder-white/70 px-4 py-3 text-base focus:outline-none focus:border-white/50 focus:bg-[#ffffff25]  appearance-none cursor-pointer"
+                                        className="w-full bg-[#ffffff1a] border border-transparent rounded-lg text-white placeholder-white/70 px-3 py-2.5 text-sm focus:outline-none focus:border-white/50 focus:bg-[#ffffff25]  appearance-none cursor-pointer"
                                         defaultValue=""
                                     >
-                                        <option value="" disabled className="text-gray-400 bg-[#19628DE0] font-sohne font-normal text-[16px] leading-[31px] tracking-normal">State Your Concern</option>
-                                        <option value="knee" className="text-white bg-[#19628DE0] font-sohne font-normal text-[16px] leading-[31px] tracking-normal">Knee Replacement</option>
-                                        <option value="joint" className="text-white bg-[#19628DE0] font-sohne font-normal text-[16px] leading-[31px] tracking-normal">Joint Replacement</option>
-                                        <option value="trauma" className="text-white bg-[#19628DE0] font-sohne font-normal text-[16px] leading-[31px] tracking-normal">Trauma and Fracture Care</option>
-                                        <option value="sports" className="text-white bg-[#19628DE0] font-sohne font-normal text-[16px] leading-[31px] tracking-normal">Sports Medicine Surgery</option>
-                                        <option value="hand" className="text-white bg-[#19628DE0] font-sohne font-normal text-[16px] leading-[31px] tracking-normal">Hand and Wrist Surgery</option>
-                                        <option value="deformity" className="text-white bg-[#19628DE0] font-sohne font-normal text-[16px] leading-[31px] tracking-normal">Deformity Correction</option>
-                                        <option value="pediatric" className="text-white bg-[#19628DE0] font-sohne font-normal text-[16px] leading-[31px] tracking-normal">Pediatric Ortho</option>
-                                        <option value="shoulder" className="text-white bg-[#19628DE0] font-sohne font-normal text-[16px] leading-[31px] tracking-normal">Shoulder Surgery</option>
+                                        <option value="" disabled className="text-gray-400 bg-[#19628DE0] font-sohne font-normal text-[14px] leading-[26px] tracking-normal">State Your Concern</option>
+                                        <option value="knee" className="text-white bg-[#19628DE0] font-sohne font-normal text-[14px] leading-[26px] tracking-normal">Knee Replacement</option>
+                                        <option value="joint" className="text-white bg-[#19628DE0] font-sohne font-normal text-[14px] leading-[26px] tracking-normal">Joint Replacement</option>
+                                        <option value="trauma" className="text-white bg-[#19628DE0] font-sohne font-normal text-[14px] leading-[26px] tracking-normal">Trauma and Fracture Care</option>
+                                        <option value="sports" className="text-white bg-[#19628DE0] font-sohne font-normal text-[14px] leading-[26px] tracking-normal">Sports Medicine Surgery</option>
+                                        <option value="hand" className="text-white bg-[#19628DE0] font-sohne font-normal text-[14px] leading-[26px] tracking-normal">Hand and Wrist Surgery</option>
+                                        <option value="deformity" className="text-white bg-[#19628DE0] font-sohne font-normal text-[14px] leading-[26px] tracking-normal">Deformity Correction</option>
+                                        <option value="pediatric" className="text-white bg-[#19628DE0] font-sohne font-normal text-[14px] leading-[26px] tracking-normal">Pediatric Ortho</option>
+                                        <option value="shoulder" className="text-white bg-[#19628DE0] font-sohne font-normal text-[14px] leading-[26px] tracking-normal">Shoulder Surgery</option>
                                     </select>
 
                                     <div className="absolute right-0 top-1/2 -translate-y-1/2 pointer-events-none pr-4">
@@ -87,10 +87,10 @@ const Ortho = () => {
                                     </div>
                                 </div>
 
-                                <div className="pt-4 text-center">
+                                <div className="pt-3 text-center">
                                     <button
                                         type="submit"
-                                        className="w-full sm:w-auto bg-[#0FB1AB33] border border-[#0FFFFFF] hover:bg-[#347d8b] text-white font-bold py-3 px-8 rounded shadow-lg uppercase tracking-wide text-sm transition-all duration-300"
+                                        className="w-full sm:w-auto bg-[#0FB1AB33] border border-[#0FFFFFF] hover:bg-[#347d8b] text-white font-bold py-2.5 px-7 rounded shadow-lg uppercase tracking-wide text-xs transition-all duration-300"
                                     >
                                         GET COST ESTIMATE
                                     </button>
